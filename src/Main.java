af045e3c709329cc0a13b0697a48422a03a360b4
plugristal <silaby84@gmail.com>
2024-03-06 18:58:02 +0100

    Reverse

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter a number");
+        Scanner scanner = new Scanner(System.in);
+        String num1 = scanner.next();
+        int number  = Integer.parseInt(num1);
+        int reversed = 0;
+
+        while(number != 0) {
+            int digit = number % 10; // берём последнюю цифру числа
+            reversed = reversed * 10 + digit; // добавляем эту цифру в обратную сторону числа
+            number = number / 10; // убираем последнюю цифру из заданного числа числа
+        }
+
+        System.out.println(reversed);
+
+
     }
 }
